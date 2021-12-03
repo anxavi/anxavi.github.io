3529b4a051b75a37da0772ccb03bc4f611d1d4bd
Anghelo Merchan <69368194+anxavi@users.noreply.github.com>
2021-12-03 20:05:10 +0100

    Add files via upload

---
+++ cloud/_constants.js
@@ -0,0 +1,6 @@
+const REF_LENGTH = 6;
+const API_KEY = "5GvaXZVLWRrTNJIc6A1kllNH4hUf4JSM4QSLaKvNY1jcoI9bgYpveZDzIGuG9RIG";
+const API_1INCH_BASE = "https://api.1inch.exchange/v3.0/97/";
+const NATIVE_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+const CASH_ADDRESS = '0xa356cfa940cc5f30233da30e95493ad9f0a58327';
+const DECIMALS = 5;
